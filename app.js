8d3ef9ac8395e11ddf14c9a54805bd8061f2c1ac
Aleksai Losey <aleksailosey@gmail.com>
2020-04-03 02:47:11 -0400

    ads

--- app.js
+++ app.js
@@ -46,4 +46,69 @@ app.use(function (req, res) {
   res.redirect('/');
 });
 
+var io = require('socket.io').listen(app, {
+    log: true,
+    origins: '*:*'
+});
+
+io.set('transports', [
+    'websocket',
+    // 'xhr-polling',
+    // 'jsonp-polling'
+]);
+
+var channels = {};
+
+io.sockets.on('connection', function (socket) {
+    var initiatorChannel = '';
+    if (!io.isConnected) {
+        io.isConnected = true;
+    }
+
+    socket.on('new-channel', function (data) {
+        if (!channels[data.channel]) {
+            initiatorChannel = data.channel;
+        }
+
+        channels[data.channel] = data.channel;
+        onNewNamespace(data.channel, data.sender);
+    });
+
+    socket.on('presence', function (channel) {
+        var isChannelPresent = !! channels[channel];
+        socket.emit('presence', isChannelPresent);
+    });
+
+    socket.on('disconnect', function (channel) {
+        if (initiatorChannel) {
+            delete channels[initiatorChannel];
+        }
+    });
+});
+
+function onNewNamespace(channel, sender) {
+    io.of('/' + channel).on('connection', function (socket) {
+        var username;
+        if (io.isConnected) {
+            io.isConnected = false;
+            socket.emit('connect', true);
+        }
+
+        socket.on('message', function (data) {
+            if (data.sender == sender) {
+                if(!username) username = data.data.sender;
+
+                socket.broadcast.emit('message', data.data);
+            }
+        });
+
+        socket.on('disconnect', function() {
+            if(username) {
+                socket.broadcast.emit('user-left', username);
+                username = null;
+            }
+        });
+    });
+}
+
 server.listen(3000);
