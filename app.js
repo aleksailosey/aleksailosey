6283a24604ea3fe217b12aa76c812b4c830a36fd
Aleksai Losey <aleksailosey@gmail.com>
2020-04-07 15:25:13 -0400

    Fixed

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-const express      = require('express'),
+const fs           = require('fs'),
+      express      = require('express'),
       app          = express(),
-      //server       = require('http').createServer(app),
+      server       = require('http').createServer(app),
       cookieParser = require('cookie-parser'),
-      path         = require('path');
+      path         = require('path'),
+      s            = require('./server.js');
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
+// x forwared for directive
+app.enable('trust proxy');
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 hbs = require('express-handlebars');
 app.engine('hbs', hbs({
   defaultLayout: 'main',
   extname: '.hbs',
+  partialsDir: 'views/',
   layoutsDir: 'views/layouts/',
-  partialsDir: 'views/partials/',
   helpers: {
-    equals: function (a, b, options) {
-      return a == b ? options.fn(this) : options.inverse(this);
-    },
-
+    equals: function (arg1, arg2, options) { return arg1 === arg2 ? options.fn(this) : options.inverse(this); },
   }
 }));
 
 app.set('view engine', 'hbs');
 
+const front    = require('./routes/front.js'),
+      running  = require('./routes/running.js'),
+      contact  = require('./routes/contact.js'),
+      books    = require('./routes/books.js'),
+      stuff    = require('./routes/stuff.js'),
+      thoughts = require('./routes/thoughts.js');
+
+app.use('/', front);
+app.use('/running', running);
+app.use('/contact', contact);
+app.use('/books', books);
+app.use('/stuff', stuff);
+app.use('/thoughts', thoughts);
+app.use('/test', require('./routes/test.js'));
+
+app.use(function (req, res) {
+  res.redirect('/');
+});
 
 
-
-// create a http server instance to listen to request
-var server = require('http').createServer(app);
-var ExpressPeerServer = require('peer').ExpressPeerServer;
-
-var options = {
-  debug: true
-}
-// peerjs is the path that the peerjs server will be connected to.
-app.use('/peerjs', ExpressPeerServer(server, options));
-// Now listen to your ip and port.
-server.listen(3000, 'localhost');
-
-const front = require('./routes/test.js');
-app.use('/test', front);
-
-// server.listen(3000);
+server.listen(3000);
